06c1aa333cf7c29042a929ece2d70a781f6615eb
Ivan Mankovich <mankovich.ivan@gmail.com>
2020-07-20 23:13:57 +0300

    Updates

---
+++ precache-manifest.d47144a446b0a578cce0b2e2bec35d3c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33cc6f31540386aea1b67d1348738e5f",
+    "url": "/leadmetesttask/index.html"
+  },
+  {
+    "revision": "9ca452904592d84ce3a3",
+    "url": "/leadmetesttask/static/css/main.41d45e29.chunk.css"
+  },
+  {
+    "revision": "4fe2e7c74073e5a18f95",
+    "url": "/leadmetesttask/static/js/2.3b9f8e1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leadmetesttask/static/js/2.3b9f8e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca452904592d84ce3a3",
+    "url": "/leadmetesttask/static/js/main.4a1ce3b9.chunk.js"
+  },
+  {
+    "revision": "4cfdea034b059a32a700",
+    "url": "/leadmetesttask/static/js/runtime-main.c27f7405.js"
+  },
+  {
+    "revision": "db122e773d3c84ffe43b343ead3c6773",
+    "url": "/leadmetesttask/static/media/Arrow 1.db122e77.svg"
+  },
+  {
+    "revision": "358d276a96aecf9f1c484d058257bbde",
+    "url": "/leadmetesttask/static/media/Bush 2.358d276a.png"
+  },
+  {
+    "revision": "6a7746e28c9726a536c1e709ac8aefac",
+    "url": "/leadmetesttask/static/media/Bush.6a7746e2.png"
+  },
+  {
+    "revision": "47f7dd26a75309995b7b6aaf438e489a",
+    "url": "/leadmetesttask/static/media/Ellipse 178.47f7dd26.png"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/leadmetesttask/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/leadmetesttask/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "cf388f1e4c656749f1e59f776cb1ec3c",
+    "url": "/leadmetesttask/static/media/Out line.cf388f1e.svg"
+  },
+  {
+    "revision": "27484c50a52c7b72d1d3969697c48619",
+    "url": "/leadmetesttask/static/media/cloud.27484c50.png"
+  },
+  {
+    "revision": "4afbd74b372d63de6ae468fb6e6cb04c",
+    "url": "/leadmetesttask/static/media/image 150.4afbd74b.png"
+  },
+  {
+    "revision": "0eeab24bebb43ab8aa05b528bd02dbed",
+    "url": "/leadmetesttask/static/media/kisspng-dairy-cattle-miglioranza-s-r-l-calf-beef-cattle-dairy-cattle-5b0dce65346ed5-1 4.0eeab24b.png"
+  }
+]);
